d0a0320f87289ede8add540a2fbb906ca62d9e9f
waydefu <123912239+waydefu@users.noreply.github.com>
2025-09-11 12:40:32 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-// firebase-messaging-sw.js
+ // firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js');
 
