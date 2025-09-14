8ae6dfe65fef0212124bb25abcb8de5b88fa0441
waydefu <123912239+waydefu@users.noreply.github.com>
2025-09-14 18:16:28 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,6 @@ import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js';
 
-// 硬編碼 Firebase 配置
 const firebaseConfig = {
 apiKey: "AIzaSyDOyp-qGQxiiBi9WC_43YFGt94kUZn7goI", // 從 Firebase 控制台獲取
 authDomain: "f-chat-wayde-fu.firebaseapp.com",
@@ -12,11 +11,6 @@ projectId: "f-chat-wayde-fu",
 appId: "1:838739455782:web:e7538f588ae374d204dbe7",
 databaseURL: "https://f-chat-wayde-fu-default-rtdb.firebaseio.com"
 };
-
-if (!firebaseConfig.messagingSenderId) {
-  console.error('Missing messagingSenderId in firebaseConfig');
-  throw new Error('Firebase configuration incomplete');
-}
 
 const app = initializeApp(firebaseConfig);
 console.log('Firebase app initialized:', app.name);
