c07ce863ef38be171cfdf8461791e91930245bee
waydefu <123912239+waydefu@users.noreply.github.com>
2025-09-14 18:07:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 import { getFirestore, collection, addDoc, onSnapshot, orderBy, query, serverTimestamp, setDoc, doc, updateDoc, arrayUnion, deleteDoc, getDoc } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 import { getDatabase, ref, onValue, onDisconnect, set as dbSet } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js';
 
-// 硬編碼 Firebase 配置
 const firebaseConfig = {
 apiKey: "AIzaSyDOyp-qGQxiiBi9WC_43YFGt94kUZn7goI", // 從 Firebase 控制台獲取
 authDomain: "f-chat-wayde-fu.firebaseapp.com",
@@ -12,11 +11,6 @@ projectId: "f-chat-wayde-fu",
 appId: "1:838739455782:web:e7538f588ae374d204dbe7",
 databaseURL: "https://f-chat-wayde-fu-default-rtdb.firebaseio.com"
 };
-
-if (!firebaseConfig.messagingSenderId) {
-  console.error('Missing messagingSenderId in firebaseConfig');
-  alert('推送通知配置錯誤，請檢查 firebaseConfig');
-}
 
 const app = initializeApp(firebaseConfig);
 console.log('Firebase initialized:', app.name);
