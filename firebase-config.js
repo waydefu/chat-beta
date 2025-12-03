24b61f987eed747140b6a046711efe371d7413c2
waydefu <123912239+waydefu@users.noreply.github.com>
2025-12-03 13:22:08 +0800

    Fix formatting and clean up firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,17 @@
-// firebase-config.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js';
 
 const firebaseConfig = {
-apiKey: "AIzaSyDOyp-qGQxiiBi9WC_43YFGt94kUZn7goI", // 從 Firebase 控制台獲取
-authDomain: "f-chat-wayde-fu.firebaseapp.com",
-projectId: "f-chat-wayde-fu",
-appId: "1:838739455782:web:e7538f588ae374d204dbe7",
-databaseURL: "https://f-chat-wayde-fu-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyDOyp-qGQxiiBi9WC_43YFGt94kUZn7goI",
+  authDomain: "f-chat-wayde-fu.firebaseapp.com",
+  projectId: "f-chat-wayde-fu",
+  appId: "1:838739455782:web:e7538f588ae374d204dbe7",
+  databaseURL: "https://f-chat-wayde-fu-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
-console.log('Firebase app initialized:', app.name);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const firestore = getFirestore(app);
